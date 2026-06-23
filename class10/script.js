e5e2f79788c2ec062f5af338758605591e40233a
Rajkumar Enduri <rajkumarkushi864@gmail.com>
2026-06-23 15:15:43 +0530

    Added HTML and CSS class examples

---
+++ class10/script.js
@@ -0,0 +1,19 @@
+
+console.log("JavaScript Working");
+alert("Hello");
+
+// let city='hyderabd';
+// city ='delhi'
+// console.log(city)
+
+// var city='hyderabd';
+// city ='delhi'
+// console.log(city)
+
+// const city='hyderabd';
+// city ='delhi'
+// console.log(city)
+
+var city = "Hyderabad";
+var city = "Delhi";
+console.log(city)
